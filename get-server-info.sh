eb8a40722bb3c7f1d11e50d7d9a263ba165323b0
Ryan Parman <ryan@ryanparman.com>
2015-07-08 23:50:24 -0700

    Update get-server-info.sh

--- get-server-info.sh
+++ get-server-info.sh
@@ -67,8 +67,8 @@ echo ""
 
 echo "HARDWARE:"
 if [ -f /proc/cpuinfo ]; then
-    echo "CPU Speed: $(cat /proc/cpuinfo | grep 'model name' | awk {'print $8'} | head -n 1)"
-    echo "CPU Cores: $(cat /proc/cpuinfo | grep 'cpu cores' | awk {'print $4}' | head -n 1)"
+    echo "CPU Speed: $(cat /proc/cpuinfo | grep 'model name' | sed -e "s/.*@ *//" | head -n 1)"
+    echo "CPU Cores: $(cat /proc/cpuinfo | grep 'cpu cores' | sed -e "s/.*: //" | head -n 1)"
 elif [[ $(which sysctl 2>&1) != *"no sysctl"* && $(which sysctl 2>&1) ]]; then
     echo "CPU Speed: $(sysctl -n machdep.cpu.brand_string | sed -e "s/.*@ *//")"
     echo "CPU Cores: $(sysctl -n hw.ncpu)"
