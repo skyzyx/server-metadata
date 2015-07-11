7169e9d367c523340550047874ecfbd29a39afc8
Ryan Parman <ryan@ryanparman.com>
2015-07-10 18:52:02 -0700

    Update get-server-info.sh

--- get-server-info.sh
+++ get-server-info.sh
@@ -104,7 +104,7 @@ if [[ $(which git 2>&1) != *"no git"* && $(which git 2>&1) ]]; then
     echo "$(git version | sed -e "s/git version/Git/" | head -n 1)"
 fi;
 if [[ $(which nano 2>&1) != *"no nano"* && $(which nano 2>&1) ]]; then
-    echo "$(nano --version | head -n 1 | sed -e "s/ (.*)//")"
+    echo "$(nano --version | head -n 1 | sed -e "s/ (.*)//" | sed -e "s/^ *//")"
 fi;
 if [[ $(which openssl 2>&1) != *"no openssl"* && $(which openssl 2>&1) ]] && [[ $(which apt-get 2>&1) != *"no apt-get"* && $(which apt-get 2>&1) ]]; then
     echo "$(apt-cache show openssl | grep 'Version:' | head -n 1 | sed 's/Version:/OpenSSL/')"
